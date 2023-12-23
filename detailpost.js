7d72bd9a740337cdad86083e487fe63583f577e5
Nguyen Van Thang <thangnv@tech.est-rouge.com>
2023-12-23 15:28:43 +0700

    Add file detailpost.js

---
+++ detailpost.js
@@ -0,0 +1,8 @@
+const detailPost = {
+    "id": "1",
+    "name": "Tin tức bóng đá",
+    "description": "Phát sóng trực tiếp trên kênh VTV3",
+    "createdAt": "xxx",
+    "updatedAt": "yyyy",
+    "deletedAt": "zzz"
+};
